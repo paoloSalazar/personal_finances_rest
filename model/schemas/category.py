cd28c2d24c5349cbd5fd6df6b0b8f07feaff96d9
PaoloSalazar <paolo.salazar@audio.com>
2025-10-27 06:22:31 -0400

    Modify model/schemas/category.py to don't require ID when creating a new category

--- model/schemas/category.py
+++ model/schemas/category.py
@@ -1,12 +1,11 @@
 from pydantic import BaseModel
 
 class CategoryBase(BaseModel):
-    id: int | None = None
     name: str
     description: str
 
 class CategoryCreate(CategoryBase):
-    id: int | None = None
+    pass
     
 class CategoryRead(CategoryBase):
     id: int | None = None
